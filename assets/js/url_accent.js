9dde9031d9c2c102c1cc66106031ca210d40a449
Joseph Durrans <joseph_durrans@outlook.com>
2020-06-30 18:56:01 +0100

    Added .htaccess and added favicon
    Added .htaccess and added favicon

--- assets/js/url_accent.js
+++ assets/js/url_accent.js
@@ -2,28 +2,28 @@ $(document).ready(function(){
     var url = window.location.pathname;
 
     switch (url) {
-        case '/index.html':
+        case '/index':
             $('nav a').each(function(){
                 if (($(this).text()).toLowerCase() == ('home').toLowerCase()) {
                     $(this).addClass('active');
                 }
             });
             break;
-        case '/about.html':
+        case '/about':
             $('nav a').each(function(){
                 if (($(this).text()).toLowerCase() == ('about').toLowerCase()) {
                     $(this).addClass('active');
                 }
             });
             break;    
-        case '/commercial-media.html':
+        case '/commercial-media':
             $('nav a').each(function(){   
                 if (($(this).text()).toLowerCase() == ('commercial media').toLowerCase()) {
                     $(this).addClass('active');
                 }
             });
             break;  
-        case '/other-media.html':
+        case '/other-media':
             $('nav a').each(function(){
                 if (($(this).text()).toLowerCase() == ('other media').toLowerCase()) {
                     $(this).addClass('active');
